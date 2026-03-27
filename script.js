d25ffc8be3a1f34fea3838b55542e832c95dc8b9
kiravi-design-lab <kiravi.design@gmail.com>
2026-03-27 21:01:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
-hamburger.addEventListener("click", (e) => {
-    e.stopPropagation(); 
-    navMenu.classList.toggle("active");
-    hamburger.classList.toggle("active");
-    document.body.classList.toggle("no-scroll");
-  });
+function closeMenu() {
+  navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  document.body.classList.remove("no-scroll");
+}
 
-  document.querySelectorAll(".nav-menu a").forEach(link => {
-    link.addEventListener("click", () => {
-      navMenu.classList.remove("active");
-      hamburger.classList.remove("active");
-      document.body.classList.remove("no-scroll");
-    });
-  });
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+});
 
-  document.addEventListener("click", function(e) {
+document.querySelectorAll(".nav-menu a").forEach(link => {
+  link.addEventListener("click", closeMenu);
+});
 
-    const isClickInsideMenu = navMenu.contains(e.target);
-    const isClickHamburger = hamburger.contains(e.target);
-
-    if (!isClickInsideMenu && !isClickHamburger) {
-      navMenu.classList.remove("active");
-      hamburger.classList.remove("active");
-      document.body.classList.remove("no-scroll");
-    }
-
-  });
+document.addEventListener("click", (e) => {
+  if (!navMenu.contains(e.target) && !hamburger.contains(e.target)) {
+    closeMenu();
+  }
+});
